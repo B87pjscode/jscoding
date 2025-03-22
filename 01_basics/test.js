dce3c52905b1ca1f53b1984a91ff04423049ae08
B87pjscode <dipi.kapandit208@gmail.com>
2025-03-22 14:35:10 +0000

    typeof

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("dipika")
+const name ="dipika"
+const Class = 6
+const roll=12
+let no=23
+var capital="Kolkata"
+let state="West Bengal"
+console.table([name,Class,roll,no,capital,state])
+
+no=78;
+capital="patna"
+state="bihar"
+console.table([name,Class,roll,no,capital,state])
