aa4bada0989480241cd377cd22b6cdd95ac9be65
B87pjscode <dipi.kapandit208@gmail.com>
2025-03-29 13:24:13 +0000

    java basics

--- 01_basics/typeof.js
+++ 01_basics/typeof.js
@@ -1,6 +1,8 @@
+let a=null
+let b=
 console.log(typeof("hello"));
 console.log(typeof(2+2))
 console.log(typeof(true))
 //console.log(typeof(false))
-console.log(typeof(sss))
-//console.log(typeof())
+console.log(typeof(a))
+console.log(typeof(b))
